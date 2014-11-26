c8937f80d079a1a628e7550424199e9f9f53542b
dempeZheng <752996510@qq.com>
2014-11-26 16:33:03 +0800

    init

---
+++ analysis-core/src/main/java/com/dempe/analysis/core/utils/ZipUtil.java
@@ -0,0 +1,63 @@
+package com.dempe.analysis.core.utils;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+
+/**
+ * @author : Dempe
+ * @version 1.0 date : 2014/11/26
+ */
+public class ZipUtil {
+
+    public static String gunzip(String compressedStr) {
+        if (compressedStr == null) {
+            return null;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ByteArrayInputStream in = null;
+        GZIPInputStream ginzip = null;
+        byte[] compressed = null;
+        String decompressed = null;
+        try {
+            compressed = new Base64().decode(compressedStr.getBytes());
+            in = new ByteArrayInputStream(compressed);
+            ginzip = new GZIPInputStream(in);
+            byte[] buffer = new byte[1024];
+            int offset = -1;
+            while ((offset = ginzip.read(buffer)) != -1) {
+                out.write(buffer, 0, offset);
+            }
+            decompressed = out.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (ginzip != null) {
+                try {
+                    ginzip.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return decompressed;
+
+    }
+}
