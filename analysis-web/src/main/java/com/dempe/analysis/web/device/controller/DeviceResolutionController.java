a2c1d969fa96955f4eaac35667c87b9055ca34bb
DempeZheng <19881110y>
2014-11-29 00:39:23 +0800

    init

---
+++ analysis-web/src/main/java/com/dempe/analysis/web/device/controller/DeviceResolutionController.java
@@ -0,0 +1,13 @@
+package com.dempe.analysis.web.device.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2014/11/29.
+ */
+@Controller
+@RequestMapping("/deviceResolution")
+public class DeviceResolutionController
+{
+}
---
+++ analysis-web/src/main/java/com/dempe/analysis/web/device/dao/DeviceCarrierDao.java
@@ -0,0 +1,22 @@
+package com.dempe.analysis.web.device.dao;
+
+import com.dempe.analysis.web.device.model.DeviceCountry;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.dao.BasicDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2014/11/29.
+ */
+@Repository
+public class DeviceCarrierDao  extends BasicDAO<DeviceCountry, Serializable> {
+
+    @Autowired
+    protected DeviceCarrierDao(Datastore dataStore) {
+        super(dataStore);
+        ensureIndexes();// 自动创建索引
+    }
+}
