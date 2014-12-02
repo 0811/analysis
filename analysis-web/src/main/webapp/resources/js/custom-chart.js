35321e3830d3d0a2a492980b591a48564f90db47
DempeZheng <19881110y>
2014-12-02 23:38:35 +0800

    init

--- analysis-web/src/main/webapp/resources/js/custom-chart.js
+++ analysis-web/src/main/webapp/resources/js/custom-chart.js
@@ -25,12 +25,11 @@ function getLineChart(div,url){
             enabled:false //用来设置是否显示‘打印’,'导出'等功能按钮，不设置时默认为显示
         },
 
-        series: [{}]
+        series: [{showInLegend: false}]
     };
 
     $.getJSON(url, function(data) {
         options.series[0].data = data;
-        options.series[0].name = "";
         var chart = new Highcharts.Chart(options);
     });
 }
