5f1d385fb40a187873be03f4b738d0a3840c433c
DempeZheng <19881110y>
2014-12-02 23:29:26 +0800

    init

--- analysis-web/src/main/webapp/resources/js/custom-chart.js
+++ analysis-web/src/main/webapp/resources/js/custom-chart.js
@@ -21,6 +21,9 @@ function getLineChart(div,url){
         credits:{
             enabled:false // 禁用版权信息
         },
+        exporting:{
+            enabled:false //用来设置是否显示‘打印’,'导出'等功能按钮，不设置时默认为显示
+        },
 
         series: [{}]
     };
