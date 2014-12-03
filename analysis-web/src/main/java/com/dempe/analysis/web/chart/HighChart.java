c1f8e7701b2c4505a3d675b3ce45d4c3f849dc77
dempeZheng <752996510@qq.com>
2014-12-03 15:07:19 +0800

    init

--- analysis-web/src/main/java/com/dempe/analysis/web/chart/HighChart.java
+++ analysis-web/src/main/java/com/dempe/analysis/web/chart/HighChart.java
@@ -1,7 +1,32 @@
 package com.dempe.analysis.web.chart;
 
+import com.alibaba.fastjson.JSONObject;
+
 /**
  * Created by Administrator on 2014/12/3.
  */
 public class HighChart {
+
+
+    private SeriesBean seriesBean;
+
+
+    private JSONObject xAxis;
+
+
+    public SeriesBean getSeriesBean() {
+        return seriesBean;
+    }
+
+    public void setSeriesBean(SeriesBean seriesBean) {
+        this.seriesBean = seriesBean;
+    }
+
+    public JSONObject getxAxis() {
+        return xAxis;
+    }
+
+    public void setxAxis(JSONObject xAxis) {
+        this.xAxis = xAxis;
+    }
 }
