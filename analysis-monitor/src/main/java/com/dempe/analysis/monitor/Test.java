dc34b993b9ac34b3bb9e0449b47301e207d45188
DempeZheng <752996510@qq.com>
2016-03-18 18:39:00 +0800

    添加app管理

---
+++ analysis-monitor/src/main/java/com/dempe/analysis/monitor/Test.java
@@ -0,0 +1,19 @@
+package com.dempe.analysis.monitor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dempe
+ * Date: 2016/3/18
+ * Time: 15:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+    public static void test(String name) {
+        System.out.println("test invoke>>>>>>>>>>>>>>>>>>>>>" + name);
+    }
+
+    public void hello() {
+
+    }
+}
